1ab7741b71c463981224e25e293bfe2c8be19600
ggba <balovaslan5@gmail.com>
2025-05-19 16:12:23 +0300

    JavaScript: timeout animation

--- js/script.js
+++ js/script.js
@@ -1,38 +1,53 @@
-// let btn1 = document.querySelector('#first');
-// let btn2 = document.querySelector('#second');
-// let btn3 = document.querySelector('#third');
-
-function HelloWorld() {
-    console.log('Hello, World!')
-}
-
-// btn1.addEventListener('click', HelloWorld);
-// btn2.addEventListener('click', HelloWorld);
-// btn3.addEventListener('click', HelloWorld);
-
 let btns = document.querySelectorAll('.nums');
-
-for (let btn of btns) {
-    btn.addEventListener('click', function () {
-        btn.textContent = Number(btn.textContent) + 1;
-    })
-};
-
+let exampleTimeOutID;
 let displayer = document.querySelector('.displayer');
 let stealth = document.querySelector('.stealth');
+let first = document.querySelector('#first');
+let second = document.querySelector('#second');
+let third = document.querySelector('#third');
+
+function displayOff() {
+    stealth.className = 'example stealth';
+}
+
+function animationTimeOut() {
+    exampleTimeOutID = window.setTimeout(displayOff, 1000);
+}
+
+// for (let btn of btns) {
+//     btn.addEventListener('click', function () {
+//         btn.textContent = Number(btn.textContent) + 1;
+//     });
+// };
+
+first.addEventListener('click', function () {
+    first.textContent = Number(first.textContent) + 1;
+});
+
+second.addEventListener('click', function () {
+    first.textContent = Number(first.textContent) + 10;
+    second.textContent = Number(second.textContent) + 10;
+});
+
+third.addEventListener('click', function () {
+    first.textContent = Number(first.textContent) + 100;
+    second.textContent = Number(second.textContent) + 100;
+    third.textContent = Number(third.textContent) + 100;
+});
 
 displayer.addEventListener('click', function () {
     if (displayer.textContent == "Отобразить") {
-        stealth.className = 'example beauty';
+        stealth.className = 'example beauty animate__animated animate__zoomInRight';
         displayer.textContent = "Скрыть";
     }else{
-        stealth.className = 'example stealth';
+        stealth.className = 'example beauty animate__animated animate__zoomOutRight';
+        animationTimeOut();
         displayer.textContent = "Отобразить";
-    }
-})
+    };
+});
 
 stealth.addEventListener('click', function () {
     for (let btn of btns) {
         btn.textContent = Number(btn.textContent) + 1000;
     };
-})
+});
